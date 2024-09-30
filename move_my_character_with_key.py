ca86e9e963b3cf16d8cecd4284fc30a502de8e19
sungals91 <sungals911@gmail.com>
2024-09-30 22:57:28 +0900

    수직 운동 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -8,11 +8,12 @@ character = character_idle
 
 running = True
 frame = 0
-dir_x = 0
+dir_x, dir_y = 0, 0
 x = 800 // 2
+y = 600 // 2
 
 def handle_events():
-    global running, dir_x, character, character_idle, character_walk
+    global running, dir_x, dir_y, character, character_idle, character_walk
 
     events = get_events()
     for event in events:
@@ -23,6 +24,10 @@ def handle_events():
                 dir_x += 1
             elif event.key == SDLK_LEFT:
                 dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -30,7 +35,11 @@ def handle_events():
                 dir_x -= 1
             elif event.key == SDLK_LEFT:
                 dir_x += 1
-        if dir_x != 0:
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
+        if dir_x != 0 or dir_y != 0:
             character = character_walk
         else:
             character = character_idle
@@ -38,11 +47,12 @@ def handle_events():
 while running:
     clear_canvas()
     background.draw(400, 300)
-    character.clip_draw(frame * 128, 0, 128, 128, x, 300)
+    character.clip_draw(frame * 128, 0, 128, 128, x, y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 6
     x += dir_x * 10
+    y += dir_y * 10
     delay(0.05)
 
 
