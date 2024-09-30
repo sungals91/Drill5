a19e0e2d146a13a56764977c62911d1252a8ed44
sungals91 <sungals911@gmail.com>
2024-09-30 22:42:54 +0900

    idle 애니메이션 출력

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,10 +5,20 @@ background = load_image('TUK_GROUND.png')
 character_idle = load_image('Idle.png')
 character_walk = load_image('Walk.png')
 
-while True:
+running = True
+frame = 0
+
+def handle_events():
+    global running
+    pass
+
+while running:
     clear_canvas()
     background.draw(400, 300)
-    character_idle.draw(400, 300)
+    character_idle.clip_draw(frame * 128, 0, 128, 128, 400, 300)
     update_canvas()
+    frame = (frame + 1) % 6
+    delay(0.05)
+
 
 close_canvas()
