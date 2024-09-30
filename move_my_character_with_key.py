7194a44f032ad8ef317856ce93a97b613e104293
sungals91 <sungals911@gmail.com>
2024-09-30 22:48:43 +0900

    애니메이션 수평 운동

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -7,17 +7,38 @@ character_walk = load_image('Walk.png')
 
 running = True
 frame = 0
+dir_x = 0
+x = 800 // 2
 
 def handle_events():
-    global running
+    global running, dir_x
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
     pass
 
 while running:
     clear_canvas()
     background.draw(400, 300)
-    character_idle.clip_draw(frame * 128, 0, 128, 128, 400, 300)
+    character_idle.clip_draw(frame * 128, 0, 128, 128, x, 300)
     update_canvas()
+    handle_events()
     frame = (frame + 1) % 6
+    x += dir_x * 10
     delay(0.05)
 
 
